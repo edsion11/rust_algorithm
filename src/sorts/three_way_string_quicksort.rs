4b194a244f648f0a9b0ede05d742754ad46c9a13
guzhenxu <B17020618@njupt.edu.cn>
2021-10-16 00:43:33 +0800

    fix: swap Function

--- src/sorts/three_way_string_quicksort.rs
+++ src/sorts/three_way_string_quicksort.rs
@@ -9,12 +9,11 @@ pub fn quick_sort(arr: Vec<&str>, lo: i32, hi: i32, d: i32) {
     let cur: u8;
     while i < hi_pointer {
         cur = char_at(arr[i], d);
-        println!("{:?}",arr);
         if cur < p {
-            swap(arr, i as usize, low_pointer as usize);
+            arr.swap(i as usize, low_pointer as usize);
             low_pointer += 1;
         } else if cur > p {
-            swap(arr, i as usize, high_pointer as usize);
+            arr.swap(i as usize, high_pointer as usize);
             high_pointer -= 1;
             i += 1;
         } else {
@@ -27,12 +26,7 @@ pub fn quick_sort(arr: Vec<&str>, lo: i32, hi: i32, d: i32) {
     }
     quick_sort(arr, high_pointer + 1, hi, d);
 }
-pub fn swap(&mut arr: Vec<&str>, i: usize, j: usize) -> Vec<&str> {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-    arr
-}
+
 pub fn char_at(str: &str, i: i32) -> u8 {
     return str.as_bytes()[i as usize] as u8;
 }
