c052d0ad8a2da438fb107a92bc40718dae1944e0
edsion11 <guzhenxu0@gmail.com>
2022-06-23 15:29:17 +0800

    feat: leetcode_143 反转+归并

--- src/javascript_solution/leetcode-143.js
+++ src/javascript_solution/leetcode-143.js
@@ -1,3 +1,20 @@
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+    this.show  = function (){
+        let cur = this;
+        while(cur){
+            process.stdout.write(String(cur.val)+' ')
+            cur = cur.next
+        }
+        process.stdout.write('\n')
+    }
+}
+
+
+
+const head = new ListNode(1,new ListNode(2,new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7,new ListNode(8,new ListNode(9)))))))))
+
 var reorderList = function(head) {
     var stash = []
     let cur = head;
@@ -18,3 +35,37 @@ var reorderList = function(head) {
     stash[i].next = null
     return head
 };
+
+var reorderList2 = function(head) {
+    let slow = head, fast = head;
+    while(fast.next&&fast.next.next){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    let mid = slow.next;
+    slow.next = null;
+    const reverseList = (head)=>{
+        if(!head||!head.next){
+            return head
+        }else{
+            let res = reverseList(head.next)
+            head.next.next = head
+            head.next = null
+            return res
+        }
+    }
+    let reversedMid = reverseList(mid)
+    let left = head;
+    let right = reversedMid;
+    while(left&&right){
+        let leftHeadNext = left.next
+        let rightHeadNext = right.next
+        left.next = right
+        left = leftHeadNext
+        right.next = left
+        right = rightHeadNext
+    }
+    return head
+};
+head.show()
+reorderList(head).show()
