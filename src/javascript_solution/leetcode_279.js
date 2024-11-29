b321086b6ea6c87e44816666e146a3faf468fc48
edsion11 <guzhenxu0@gmail.com>
2024-11-29 23:59:57 +0800

    feat: 279

--- src/javascript_solution/leetcode_279.js
+++ src/javascript_solution/leetcode_279.js
@@ -9,7 +9,15 @@
  * @return {number}
  */
 var numSquares = function(n) {
-    
+    const f = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+        let minn = Number.MAX_VALUE;
+        for (let j = 1; j * j <= i; j++) {
+            minn = Math.min(minn, f[i - j * j]);
+        }
+        f[i] = minn + 1;
+    }
+    return f[n];
 };
 console.log(numSquares(12)); // 3
 console.log(numSquares(13)); // 2
