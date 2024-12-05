00b175b7130096f558f2d38c34d712e37ac1fa57
edsion11 <guzhenxu0@gmail.com>
2024-12-05 21:30:42 +0800

    feat: 221,541

---
+++ src/javascript_solution/leetcode_221.js
@@ -0,0 +1,50 @@
+/**
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ */
+
+
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function (matrix) {
+    const dp = new Array(matrix.length).fill(0).map(() => new Array(matrix[0].length).fill(0));
+    for (let i = 0; i < matrix.length; i++) {
+        dp[i][0] = matrix[i][0] === '1' ? 1 : 0;
+    }
+    for (let i = 0; i < matrix[0].length; i++) {
+        dp[0][i] = matrix[0][i] === '1' ? 1 : 0;
+    }
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[0].length; j++) {
+            if (matrix[i][j] === '1') {
+                if (dp[i - 1][j - 1] === 1) {
+                    dp[i][j] = dp[i-1][j-1]
+                    const len = dp[i - 1][j - 1] ** 0.5;
+                    for (let k = j; k >= j - len; k--) {
+                        if (matrix[i][k] === '1') {
+                            dp[i][j]++
+                        }
+                    }
+                    for (let k = i; k >= i - len; k--) {
+                        if (matrix[k][j] === '1') {
+                            dp[i][j]++
+                        }
+                    }
+                    if(dp[i][j]-1===(len+1)**2){
+                        dp[i][j] = dp[i][j]-1
+                    }else{
+                        dp[i][j] = 1
+                    }
+                }
+            }
+        }
+    }
+    return Math.max(...dp.flat())
+};
+console.log(maximalSquare([["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]])) // 4
+console.log(maximalSquare([["0","1"],["1","0"]])) // 1
+console.log(maximalSquare([["0"]])) // 0
+console.log(maximalSquare([["1","1"],["1","1"]])) // 1
+
+// []
---
+++ src/javascript_solution/leetcode_541.js
@@ -0,0 +1,54 @@
+/**
+ * 给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+ * 如果剩余字符少于 k 个，则将剩余字符全部反转。
+ * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ */
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+    const reverse = (str) => {
+        return str.split('').reverse().join('');
+    }
+    if (s.length < k) {
+        return reverse(s);
+    }
+    let res = ''
+    // const n = Math.floor(s.length/(2*k))
+    while (s.length - res.length >= 2 * k) {
+        res = res + reverse(s.slice(res.length, res.length + k)) + s.slice(res.length + k, res.length + 2 * k)
+    }
+    const len = s.length - res.length
+    if (len < 2 * k && len > 0) {
+        if (len < k) {
+            res = res + reverse(s.slice(res.length))
+        } else {
+            res = res + reverse(s.slice(res.length, res.length + k)) + s.slice(res.length + k)
+        }
+    }
+    return res
+}
+// 循环遍历字符串，每次取出2k个字符，前k个字符反转，后k个字符不变
+var reverseStr = function(s, k) {
+    const n = s.length;
+    const arr = Array.from(s);
+    for (let i = 0; i < n; i += 2 * k) {
+        reverse(arr, i, Math.min(i + k, n) - 1);
+    }
+    return arr.join('');
+};
+
+const reverse = (arr, left, right) => {
+    while (left < right) {
+        const temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+}
+console.log(reverseStr("abcdefg", 2)); // "bacdfeg"
+console.log(reverseStr("abcd", 2)); // "bacd"
