64e35b2476a94eeefddf0cd05ee64bb946e83aa1
edsion11 <guzhenxu0@gmail.com>
2024-12-08 16:16:21 +0800

    fix: 300, 1143

--- src/javascript_solution/leetcode_1143.js
+++ src/javascript_solution/leetcode_1143.js
@@ -19,6 +19,22 @@ var longestCommonSubsequence = function (text1, text2) {
   return dp[m - 1][n - 1];
 };
 
+var longestCommonSubsequence = function(text1, text2) {
+    const m = text1.length;
+    const n = text2.length;
+    const dp = new Array(m+1).fill(0).map(item=>new Array(n+1).fill(0));
+    for( let i=1;i<=m;i++){
+        for(let j=1;j<=n;j++){
+            if(text1[i-1]===text2[j-1]){
+                dp[i][j] = dp[i-1][j-1]+1
+            }else{
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    return dp[m][n]
+};
+
 console.log(longestCommonSubsequence("abab", "baba")); // 3
 console.log(longestCommonSubsequence("abcde", "ace")); // 3
 console.log(longestCommonSubsequence("abc", "def")); // 0
--- src/javascript_solution/leetcode_300.js
+++ src/javascript_solution/leetcode_300.js
@@ -3,21 +3,25 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-  if (nums.length == 0) {
-    return 0;
-  }
-  var dp = [1];
-  var max = 1 ;
-  for (let i = 0; i < nums.length; i++) {
-      dp[i]=1
-    for (let j=0;j < i; j++){
-        if(nums[j]<nums[i]){
-            dp[i] = Math.max(dp[i], dp[j]+1)
-        }
+    if (nums.length == 0) {
+        return 0;
     }
-    max = Math.max(max,dp[i])
-  }
-  return max
+    // dp[i]表示以nums[i]结尾的最长上升子序列的长度
+    var dp = [1];
+    var max = 1;
+    for (let i = 0; i < nums.length; i++) {
+        dp[i] = 1
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1)
+            }
+        }
+        max = Math.max(max, dp[i])
+    }
+    return max
 };
 
-console.log(lengthOfLIS([10,9,2,5,3,7,101,18]))
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])) // 4
+console.log(lengthOfLIS([1,7,8,3,4])) // 3
+console.log(lengthOfLIS([1,7,8,3,4,5])) // 4
+// [1,7,8,3,4]
