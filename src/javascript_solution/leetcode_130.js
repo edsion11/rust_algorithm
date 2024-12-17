b3f0ffea7cc83ce0e120fed8901be777ca6d228d
edsion11 <guzhenxu0@gmail.com>
2024-12-17 18:30:19 +0800

    feat: 130

--- src/javascript_solution/leetcode_130.js
+++ src/javascript_solution/leetcode_130.js
@@ -3,28 +3,35 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 const array = [["X","O","X"],["X","O","X"],["X","O","X"]]
-console.log(array)
+// 思路： 从边界开始，将边界上的O以及与边界上的O相连的O全部标记为A，然后遍历整个数组，将O标记为X，将A标记为O
 var solve = function(board) {
-    for (let i=0; i<board.length; i++) {
-        for (let j=0; j<board[i].length; j++) {
-            if(board[i][j] === 'O'&& !(j===0||j===board[i].length-1||i===0||i===board.length-1)){
-                console.log(i ,j)
-                if(!(i-1===0&&board[i-1][j]==='O')){
-                    console.log(1);
-                    board[i][j] = 'X'
-                }
-                if(!(i+1===board.length-1&&board[i+1][j]==='O')){
-                    console.log(2);
-                    board[i][j] = 'X'
-                }
-                if(!(j-1===0&&board[i][j-1]==='O')){
-                    console.log(3);
-                    board[i][j] = 'X'
-                }
-                if(!(j+1===board[i].length-1&&board[i][j+1]==='O')){
-                    console.log(4);
-                    board[i][j] = 'X'
-                }
+    const row = board.length
+    const col = board[0].length
+    const dfs = (i, j) => {
+        if (i < 0 || i >= row || j < 0 || j >= col || board[i][j] !== 'O') {
+            return
+        }
+        board[i][j] = 'A'
+        dfs(i + 1, j)
+        dfs(i - 1, j)
+        dfs(i, j + 1)
+        dfs(i, j - 1)
+    }
+    for (let i = 0; i < row; i++) {
+        dfs(i, 0)
+        dfs(i, col - 1)
+    }
+    for (let i = 0; i < col; i++) {
+        dfs(0, i)
+        dfs(row - 1, i)
+    }
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (board[i][j] === 'O') {
+                board[i][j] = 'X'
+            }
+            if (board[i][j] === 'A') {
+                board[i][j] = 'O'
             }
         }
     }
