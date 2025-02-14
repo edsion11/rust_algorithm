1622d28a3225ce4e15c01cbd6c7dcfd1562125b6
edsion11 <guzhenxu0@gmail.com>
2025-02-14 23:18:46 +0800

    feat: 543

---
+++ src/javascript_solution/leetcode_543.js
@@ -0,0 +1,30 @@
+/**
+ * 给你一棵二叉树的根节点，返回该树的 直径 。
+ * 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+ * 两节点之间路径的 长度 由它们之间边数表示。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function (root) {
+  let res = 0;
+  const getDepth = (node) => {
+    if (!node) return 0;
+    const left = getDepth(node.left);
+    const right = getDepth(node.right);
+    res = Math.max(res, left + right);
+    return Math.max(left, right) + 1;
+  };
+    getDepth(root); 
+  return res
+};
