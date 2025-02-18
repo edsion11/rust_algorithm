04f462ee44b589876f22126b2eaec3c7b64a526b
edsion11 <guzhenxu0@gmail.com>
2025-02-18 23:11:17 +0800

    feat: 79

--- src/javascript_solution/leetcode_79.js
+++ src/javascript_solution/leetcode_79.js
@@ -43,6 +43,33 @@ var exist = function (board, word) {
     return false;
 };
 
+
+var exist = function (board, word) {
+    const m = board.length
+    const n = board[0].length
+    const backtrack = (i, j, k) => {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] !== word[k]) {
+            return false
+        }
+        if (k === word.length - 1) {
+            return true
+        }
+        const temp = board[i][j]
+        board[i][j] = ''
+        const res = backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)
+        board[i][j] = temp
+        return res
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (backtrack(i, j, 0)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
 // console.log(
 //     exist(
 //         [
